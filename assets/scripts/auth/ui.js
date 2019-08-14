564f83e3f2372208a9ef9f49a6cb23a74bb7344b
marcpelve <marc.pelve@gmail.com>
2019-08-14 11:16:25 -0400

    Update to show ui components
    Added ui file containing user feedback and form clears.

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const store = require('./../store')
+
+const signUpSuccess = data => {
+  $('#message').text('Successfully signed up.')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  console.log('signUpSuccess ran')
+
+  $('form').trigger('reset')
+}
+
+const signInSuccess = data => {
+  store.user = data.user
+
+  $('#message').text('Successfully signed in.')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  console.log('signInSuccess ran. User is', store.user)
+
+  $('#signed-in-user').text('User: ' + store.user.email)
+
+  $('form').trigger('reset')
+}
+
+const changePasswordSuccess = data => {
+  $('#message').text('Successfully changed password.')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  console.log('changePasswordSuccess ran')
+
+  $('form').trigger('reset')
+}
+
+const signOutSuccess = data => {
+  store.user = null
+  $('#message').text('Successfully signed out.')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  console.log('signOutSuccess ran')
+
+  $('#signed-in-user').text('')
+
+  $('form').trigger('reset')
+}
+
+const failure = data => {
+  $('#message').text('Operation error')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  console.error('failure ran')
+
+  $('form').trigger('reset')
+}
+
+module.exports = {
+  signUpSuccess,
+  signInSuccess,
+  changePasswordSuccess,
+  signOutSuccess,
+  failure
+}
